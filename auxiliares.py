c43179506eeb8f9a63d92008dde1f1b793841a0f
Magno Lomardo <magnussolidus@gmail.com>
2021-04-26 02:56:22 -0300

    fix: parênteses faltando

--- auxiliares.py
+++ auxiliares.py
@@ -79,7 +79,7 @@ class Peca:
             else:
                 return "PT";
 
-        elif(tipo == TipoPeca.RAINHA:
+        elif(tipo == TipoPeca.RAINHA):
             if(cor == CorPeca.BRANCA):
                 return "BQ";
             else:
